c1aa4022505a1b2e424605924ac75a37835d9ccb
Drigter1999 <yah9600@gmail.com>
2022-03-09 01:59:59 +0900

    성공

---
+++ app/src/main/java/kr/ac/yeonsung/yah9600/yaho/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.yeonsung.yah9600.yaho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
